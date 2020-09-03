2e546e21fda8b72bae754cd498d63e3098357740
illiaharkavy <igorkovoy@gmail.com>
2020-09-03 19:27:44 +0300

    Calculator interface

---
+++ Calculator/Calculator/CalculatorViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by ILLIA HARKAVY on 9/3/20.
+//  Copyright © 2020 ILLIA HARKAVY. All rights reserved.
+//
+
+import UIKit
+
+class CalculatorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
